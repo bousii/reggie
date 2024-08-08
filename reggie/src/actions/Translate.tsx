39adb78d070b0abe98ff824cb61a48f0093270ac
bousii <owen.a.giles@gmail.com>
2024-08-08 16:26:46 -0500

    Initial algo commit

--- reggie/src/actions/Translate.tsx
+++ reggie/src/actions/Translate.tsx
@@ -1,9 +1,3 @@
 export default function Translate(input: string) {
-  if (input.toLowerCase().includes("reggie") && input.length <= 500) {
-    return "REGGIE!";
-  } else if (input.length <= 500) {
-    return input;
-  } else {
-    return "Yapster";
-  }
+  return "Invalid Syntax";
 }
