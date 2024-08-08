90debe588777a66979b333733c255d6c2456493d
Anthony <107648652+LckyClovr@users.noreply.github.com>
2024-08-08 01:24:40 -0500

    init fr this time

---
+++ reggie/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <h1 className="flex text-red-500 text-3xl">Reggie</h1>
+      <p>Yoooo</p>
+    </main>
+  );
+}
